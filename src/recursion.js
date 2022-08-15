439870b610d794cf30a75b9ee6f20fb76d967065
Andy Simmons <andy116@vt.edu>
2022-08-15 12:14:31 -0400

    save whiel trying to make work

--- src/recursion.js
+++ src/recursion.js
@@ -6,6 +6,7 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
+
 var factorial = function(n) {
   if (n < 0){
     return null;
